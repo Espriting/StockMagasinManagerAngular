186f337146687caf475227b8ac82a7d9d52cc960
Azriiii <47857678+Azriiii@users.noreply.github.com>
2021-12-05 15:51:22 +0100

    token

---
+++ src/app/User-Management/service/auth.service.ts
@@ -0,0 +1,89 @@
+import {Injectable} from '@angular/core';
+import {User} from "../model/User";
+import {Router} from "@angular/router";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {JwtHelperService} from "@auth0/angular-jwt";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthService {
+    apiURL: string = 'http://localhost:8117/StockMagasinManager';
+    token: string;
+    public loggedUser: string;
+    public isloggedIn: Boolean = false;
+    public roles: string[];
+    private helper = new JwtHelperService();
+
+    constructor(private router: Router, private http: HttpClient) {
+    }
+
+
+    login(user: User) {
+        return this.http.post<User>(this.apiURL + '/login', user, {observe: 'response'});
+    }
+
+    saveToken(access_token: string) {
+        localStorage.setItem('access_token', access_token);
+        this.token = access_token;
+        this.isloggedIn = true;
+        this.decodeJWT();
+    }
+
+    decodeJWT() {
+        if (this.token == undefined)
+            return;
+        const decodedToken = this.helper.decodeToken(this.token);
+        this.roles = decodedToken.roles;
+        this.loggedUser = decodedToken.sub;
+    }
+
+    loadToken() {
+        this.token = <string>localStorage.getItem('access_token');
+        this.decodeJWT();
+    }
+
+    logout() {
+        this.loggedUser = this.loggedUser;
+        this.roles = this.roles;
+        this.token = this.token;
+        this.isloggedIn = false;
+        localStorage.removeItem('access_token');
+        this.router.navigate(['/login']);
+    }
+
+    isTokenExpired(): Boolean {
+        return this.helper.isTokenExpired(this.token);
+    }
+
+    // getToken(): string {
+    //     return this.token;
+    // }
+
+    setLoggedUserFromLocalStorage(login: string) {
+        this.loggedUser = login;
+        this.isloggedIn = true;
+        this.getUserRoles(login);
+    }
+
+    getUserRoles(login: string) {
+
+    }
+
+    generatetoken(nom:any, password:any){
+        let body = new URLSearchParams();
+        body.set('nom',nom);
+        body.set('password',password);
+        const httpOptions = {
+            headers: new HttpHeaders({
+                authorization: 'my-auth-token'
+            })
+        };
+        console.log(body.toString())
+        httpOptions.headers =httpOptions.headers.set('Content-type', 'application/x-www-form-urlencoded');
+        return this.http.post('http://localhost:8117/StockMagasinManager/login',body.toString(),httpOptions);
+
+
+    }
+
+}
---
+++ src/app/User-Management/service/user.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../model/User';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    private baseURL=environment.url+"user";
+    constructor(private HttpClient: HttpClient) { }
+
+    getUsersList(): Observable<any>{
+        return this.HttpClient.get(this.baseURL+'/users');
+    }
+
+    addUser(user: User){
+        return this.HttpClient.post(this.baseURL+'/registerUser',user);
+    }
+
+    updateUser(id: number,user: User){
+        return this.HttpClient.put(this.baseURL+'/updateUser/${id}',user);
+    }
+
+    deleteUser(id: number){
+        return this.HttpClient.delete(this.baseURL+'/deletUser/${id}'+id);
+    }
+
+    getUserById(id: number){
+        return this.HttpClient.get(this.baseURL+'/getUserById/${id}'+id);
+    }
+    login(user: User){
+        return this.HttpClient.post(this.baseURL+'/login',user);
+    }
+    register(user: User){
+        return this.HttpClient.put(this.baseURL+'/registerUser',user);
+    }
+}
