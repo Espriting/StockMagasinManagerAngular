2d8a068282617184ca46f1314e8a2bb59980f510
monambengouta <monam.bengouta@esprit.tn>
2021-12-14 00:38:42 +0100

    ajouter filter metier

--- src/app/StockManagement/Stock/add-stock/add-stock.component.ts
+++ src/app/StockManagement/Stock/add-stock/add-stock.component.ts
@@ -3,7 +3,7 @@ import { Product } from 'src/app/model/product';
 import { Stock } from 'src/app/model/Stock';
 import { ProductService } from 'src/app/Product-Management/service/product.service';
 import { DataSharedStockService } from '../../Service/data-shared-stock.service';
-import { FormControl, FormGroup, Validators,ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { FormControl, FormGroup, Validators,ReactiveFormsModule, FormBuilder, Form, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-stock',
@@ -11,44 +11,37 @@ import { FormControl, FormGroup, Validators,ReactiveFormsModule, FormBuilder } f
   styleUrls: ['./add-stock.component.css']
 })
 export class AddStockComponent implements OnInit {
-stock:Stock;
-selectedOption:String;
+@Input() stock:Stock;
 listProduct:Product[];
 listProductLibelle:String[];
-buttonsaveInformation:boolean;
 validatingForm: FormGroup;
 @Output() addEvent=new EventEmitter<Stock>();
+//@Output() hideForm=new EventEmitter<Boolean>();
   constructor( private stockservice:DataSharedStockService,private produitService:ProductService) { }
 
   ngOnInit(): void {
     this.produitService.getProductsList().subscribe(
       (data)=>{
         this.listProduct=data;
-      }//,(err)=>console.log(err),
-      //()=>this.listProduct.map((elem)=>elem.code)
+      }
       
-    ) 
-    this.stock=new Stock();
+    )  
     this.validatingForm = new FormGroup({ //form to add stock 
       'libelle': new FormControl('',[Validators.required,Validators.minLength(16)]),
-      'quantity' :new FormGroup({
-        'qt':new FormControl ('',[Validators.required,Validators.pattern("[0-9]{8}")]),
-        'qtmin':new FormControl ('',[Validators.required,Validators.pattern("[0-9]{8}")]),
-      //  'productCode':new FormBuilder().group({productName:[this.listProduct,[Validators.required]]})
-      })
-    });
+        'qt':new FormControl ('',[Validators.required,Validators.pattern("[0-9]+")]),
+        'qtmin':new FormControl ('',[Validators.required,Validators.pattern("[0-9]+")]),
+       'productCode':new FormControl('',Validators.required)});
+    this.stock=new Stock();
   }
  
 
-    save(){  
-      console.log(this.stock+"this is ur stock");
-      this.listProduct= this.listProduct.filter((elem)=>elem.code==this.selectedOption);//filtre list product with code=slected option
-      this.stock.produit=this.listProduct;//initialize product
-      this.stockservice.addStock(this.stock);//send product to backend
-      this.addEvent.emit(this.stock);
-      
-    }
-    onSubmit() {
-      console.log(JSON.stringify(this.validatingForm.value))
+    save(myForm:FormGroup){  
+      this.stock.libelleStock=myForm.controls['libelle'].value;
+      this.stock.qte=myForm.controls['qt'].value;
+      this.stock.qteMin=myForm.controls['qtmin'].value;
+      //this.listProduct= this.listProduct.filter((elem)=>elem.code==this.validatingForm.controls['productCode'].value);//filtre list product with code=slected option
+      //this.stock.produit=this.listProduct;//initialize product
+      this.stockservice.addStock(this.stock).subscribe();//send product to backend
+      this.addEvent.emit(this.stock);  
     }
 }
